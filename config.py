0e070fa55c4eb2fbe651f2b657ce16d9fde19ac2
itamarafrimi <itamar.afrimi@mail.huji.ac.il>
2024-06-16 17:26:30 +0300

    Removing heroku option

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard_to_guess_string'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
